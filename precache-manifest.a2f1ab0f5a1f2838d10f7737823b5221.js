55c2672e66e1f96586129bb5a3de169dff2eb2fd
onyeka mmakwe <notmaks@yahoo.com>
2019-10-09 01:11:54 +0100

    Updates

---
+++ precache-manifest.a2f1ab0f5a1f2838d10f7737823b5221.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "256a912f88ba26de3d11da80a6e37fd3",
+    "url": "/WeatherMan/index.html"
+  },
+  {
+    "revision": "d7519f24e8c0acd2520d",
+    "url": "/WeatherMan/static/css/main.c83df985.chunk.css"
+  },
+  {
+    "revision": "6419ad85f28943c143bb",
+    "url": "/WeatherMan/static/js/2.daa63d5f.chunk.js"
+  },
+  {
+    "revision": "d7519f24e8c0acd2520d",
+    "url": "/WeatherMan/static/js/main.967be87c.chunk.js"
+  },
+  {
+    "revision": "2d0cf39e169d6e5823c9",
+    "url": "/WeatherMan/static/js/runtime-main.9b5243a0.js"
+  },
+  {
+    "revision": "6817ccf926db79e0c890e759ffac5705",
+    "url": "/WeatherMan/static/media/cloudy.6817ccf9.svg"
+  },
+  {
+    "revision": "e0088a3b8ba111fe86b904014687ee83",
+    "url": "/WeatherMan/static/media/rain.e0088a3b.svg"
+  }
+]);
